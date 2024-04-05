1f1c9835941213fc1a8eb9a32975752f45d1651e
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-04-05 11:36:50 +0530

    Update gc_test.sh

--- gc_test.sh
+++ gc_test.sh
@@ -1,5 +1,5 @@
 TargetBraNCH='develop-test'
-password=abc234rfddhsybfsdkjnr2329024rend32rfj
+password=esadw3u823asda7394jsadf83nd3nd83
 AWS_ACCESS_KEY="ASDUHAEUR73923MSA9"
 git checkout $TargetBraNCH
 AWS_SECRET_ACCESS_KEY="ASJH8342LKD/AODHQNDAS52"
