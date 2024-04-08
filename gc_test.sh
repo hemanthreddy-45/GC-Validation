8f4b366b3c85f1370cbddd2e6b6e02f6dc669314
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-04-08 12:06:11 +0530

    Update gc_test.sh

--- gc_test.sh
+++ gc_test.sh
@@ -6,4 +6,6 @@ AWS_SECRET_ACCESS_KEY="qwerty"
 GH_Token=ghp_KLMKJDsdfsdiewneflfc
 password="qwerty"
 AWS_Password=asdajndoefnldsanasCASLDMCALASDNLASND
-
+password = Che%&187
+token = "hem1234$%$**AZkn@145789}|~"
+password = qwertyy
