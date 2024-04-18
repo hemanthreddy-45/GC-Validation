b7908515bee027d8be71272be4bfd68b595bd7c9
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-04-18 17:36:20 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,6 +1,6 @@
 password=P$5aRt#2oN!9lM@8
 token=P$5aRt#2oN!9lM@8
-password=P$5aRt#2oN!9lM@8
+password="P$5aRt#2oN!9lM@8"
 token = "P$5aRt#2oN!9lM@8"
 key = "P$5aRt#2oN!9lM@8"
 hello = "P$5aRt#2oN!9lM@8"
