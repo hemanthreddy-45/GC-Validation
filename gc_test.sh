f3bcd69a4200a9501082b2d522cba25a501a1a2a
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-05-09 22:09:55 +0530

    Update gc_test.sh

--- gc_test.sh
+++ gc_test.sh
@@ -1,13 +1,6 @@
 TargetBraNCH='develop-test'
-password=qwertyasdawda
+password="qwertyasdawda"
 AWS_ACCESS_KEY="ASDUHAEUR73923MSA9"
 git checkout $TargetBraNCH
 AWS_SECRET_ACCESS_KEY="qwerty"
 GH_Token=ghp_KLMKJDsdfsdiewneflfc
-password="qwerty"
-AWS_Password=asdajndoefnldsanasCASLDMCALASDNLASND
-password = Che%&187
-token = "hem1234$%$**AZkn@145789}|~"
-password = qwertyy$
-token = "P$5aRt#2oN!9lM@8as"
-
